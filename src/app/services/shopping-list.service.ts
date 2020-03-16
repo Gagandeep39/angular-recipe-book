ee88816fa8192dbdd8dc930368eb17d42847bfcd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 12:01:17 +0530

    Create an emitter to inform the list that data has changed Fixed : ShoppingList

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../models/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
+
+  //Whenever a new ingredient is added, a new array will be passed to the emitter's subscriber
+  @Output() ingredientsChanged = new EventEmitter<Ingredient[]>();
 
   ingredients : Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -13,11 +16,14 @@ export class ShoppingListService {
 
   constructor() { }
 
+  // We can pass data without slice if we ensure that it isnt edited outside in a wrong way
+  // Better approach would to to use an emitter to inform that list has changed
   getIngredients(){
     return this.ingredients.slice();
   }
 
   addIngredients(newIngredient : Ingredient){
     this.ingredients.push(newIngredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
