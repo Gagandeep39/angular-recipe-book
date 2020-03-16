221908418de16c5c925ac51289a966bfb084089d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-16 12:10:19 +0530

    Recipe Model now consists of Ingredient array Modified Dummy data in recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,29 +1,46 @@
-import { Injectable, Output, EventEmitter } from '@angular/core';
-import { Recipe } from '../models/recipe.model';
+import { Injectable, Output, EventEmitter } from "@angular/core";
+import { Recipe } from "../models/recipe.model";
+import { Ingredient } from "../models/ingredient.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class RecipeService {
-
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a Simple Recipe', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/11/beet.jpg'),
-    new Recipe('Test Recipe 2', 'This is a Simple Recipe 2', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/11/beet.jpg')
+    new Recipe(
+      "Burger",
+      "A hamburger (also burger for short) is a food consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1200px-RedDot_Burger.jpg",
+      [
+        new Ingredient("Bread", 2),
+        new Ingredient("Cabbage", 1)
+      ]
+    ),
+    new Recipe(
+      "Pasta",
+      "Pasta is a type of food typically made from an unleavened dough of durum wheat flour",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg/377px-%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg",
+      [
+        new Ingredient("Wheat", 1),
+        new Ingredient("Eggs", 2),
+        new Ingredient("Flour", 1)
+      ]
+    )
   ];
 
-  constructor() { }
+  constructor() {}
 
-  // Slice with no param returns a copy of an array 
-  // Without splice, the method will pass an array reference 
-  // Any data modified by components will be modified in services too 
-  // To prefent this behaviour, slice is added 
-  getRecipes(){
+  // Slice with no param returns a copy of an array
+  // Without splice, the method will pass an array reference
+  // Any data modified by components will be modified in services too
+  // To prefent this behaviour, slice is added
+  getRecipes() {
     return this.recipes.slice();
   }
 
-  addRecipe(recipe : Recipe){
+  addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
   }
 }
