e1de464c77b4dada8254c5a8e3ac64fc93467a0a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-17 14:56:48 +0530

    RecipeEdit now fetch Parameterers via Routing

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  selector: "app-recipe-edit",
+  templateUrl: "./recipe-edit.component.html",
+  styleUrls: ["./recipe-edit.component.css"]
 })
 export class RecipeEditComponent implements OnInit {
+  index: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private service: ActivatedRoute) {}
 
   ngOnInit() {
+    this.service.params.subscribe((params: Params) => {
+      this.index = params["id"];
+      this.editMode = params["id"] != null; // Will be true if id exists, false when id oesnt exist and we create a new recipe
+    });
   }
-
 }
