57abab6c542f9c67b25fc00a6d0e117e815a6859
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-17 12:31:10 +0530

    Routin implemented for parent component / now redirects to recipes

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -4,8 +4,11 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    { path: '', component: RecipesComponent },
-    { path: 'shoppinglist', component: ShoppingListComponent }
+    // pathMatc: 'full' specifies if the path is completely empty then redirect to recipe 
+    // This is because '' is present in every path
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shoppinglist', component: ShoppingListComponent },
 ]
 
 @NgModule({
