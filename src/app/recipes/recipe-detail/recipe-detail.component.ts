2e7770d00a22135d1d94f3b6a4e0b91a0bff1090
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-17 14:57:37 +0530

    Added various possible Routing path Edit Recipe Button now routes to EditRecipe Component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,21 +11,28 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeDetailComponent implements OnInit {
 
   // @Input() recipe : Recipe;
-  @Input() index: number;
+  index: number;
   recipe: Recipe;
 
-  constructor(private recipeService : RecipeService, private route: ActivatedRoute) { 
+  constructor(private recipeService : RecipeService, private route: ActivatedRoute, private router: Router) { 
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params)=>{
-      const id = +params['id'];
-      this.recipe = this.recipeService.getRecipeById(id);
+      this.index = +params['id'];
+      this.recipe = this.recipeService.getRecipeById(this.index);
     })
   }
 
   addToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  editData(){
+    // Possibility 1
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // Possibility 2
+    // this.router.navigate(['../', this.index, 'edit'], {relativeTo: this.route});
   }
 
 }
