955e43b385ed7e41ab158590252c5d1f58538b26
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-17 13:46:00 +0530

    Added routing approch to fetch recipe details by id

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,11 +10,17 @@ import { RecipeService } from 'src/app/services/recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
 
-  @Input() recipe : Recipe;
+  // @Input() recipe : Recipe;
+  recipe: Recipe;
 
-  constructor(private recipeService : RecipeService) { }
+  constructor(private recipeService : RecipeService, private route: ActivatedRoute) { 
+  }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params)=>{
+      const id = +params['id'];
+      this.recipe = this.recipeService.getRecipeById(id);
+    })
   }
 
   addToShoppingList(){
