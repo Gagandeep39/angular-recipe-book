2faf7d163c0a38823bbfa302dbc81e8be6e4df21
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-18 15:39:29 +0530

    Created Reacive form for New Recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-recipe-edit",
@@ -9,13 +10,29 @@ import { ActivatedRoute, Params } from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   index: number;
   editMode = false;
+  recipeForm: FormGroup;
+  submitted = false;
 
-  constructor(private service: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.service.params.subscribe((params: Params) => {
+    this.route.params.subscribe((params: Params) => {
       this.index = params["id"];
       this.editMode = params["id"] != null; // Will be true if id exists, false when id oesnt exist and we create a new recipe
     });
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl('', Validators.required),
+      'description': new FormControl('', [Validators.required, Validators.minLength(20)]),
+      'imagePath': new FormControl('', Validators.required)
+    });
+  }
+
+  submitForm() {
+    this.submitted = true;
+    if (this.recipeForm.valid) {
+      this.editMode = false;
+      this.submitted = false;
+    }
   }
 }
