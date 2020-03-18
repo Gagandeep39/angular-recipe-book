dac33bfeee6fcf0a80285e385b5c54762713b88d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-18 23:38:28 +0530

    Optimised code: Unsubscribed  ondestroy in recipelist

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[] = [];
+  recipeSubscription: Subscription;
 
   constructor(private recipeService : RecipeService) { }
 
   ngOnInit() {
-    this.recipeService.recipeChanged.subscribe((newRecipes: Recipe[]) => {
+    this.recipeSubscription = this.recipeService.recipeChanged.subscribe((newRecipes: Recipe[]) => {
       this.recipes = newRecipes;
     });
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  ngOnDestroy(){
+    this.recipeSubscription.unsubscribe();
   }
 
 }
