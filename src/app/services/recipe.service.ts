239baf8235ce8c38e1d34b885b810fe2a5dfe91e
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-18 22:35:16 +0530

    Added delete recipe code

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -64,5 +64,8 @@ export class RecipeService {
     this.recipeChanged.next(this.recipes.slice());
   }
 
-  deleteRecipe(index: number) {}
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
+  }
 }
