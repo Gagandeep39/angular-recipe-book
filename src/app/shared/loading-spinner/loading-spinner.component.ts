a27c6f4fffbd1be1bfa4614181a9466bda96b0a9
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 02:52:43 +0530

    Created a loading spinner component

---
+++ src/app/shared/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  template: `
+    <div class="loadingio-spinner-spinner-isrejrtj0r">
+      <div class="ldio-1o78x5pt58d">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
