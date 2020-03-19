8ca8542626d928ca7490c7b8592b7fe477dddf5a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 00:14:08 +0530

    Improved code to manage empty ingredient list

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -2,6 +2,7 @@ import { RecipeService } from './recipe.service';
 import { Recipe } from './../models/recipe.model';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,33 @@ export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   fetchDataFromServer() {
-    return this.http.get(this.server_url).subscribe(
-      (responseList: Recipe[]) => {
-        this.recipeService.setRecipes(responseList);
-      },
-      error => {
-        console.log(error);
-      },
-      () => {
-        console.log('Fetched data from Server');
-      }
-    );
+    return this.http
+      .get<Recipe[]>(this.server_url)
+      .pipe(
+        // To mprove the code and prevent breaking because of no ingredients(even though it ws working fine before)
+        map(response => {
+          // Lets us transform data
+          return response.map(recipe => {
+            return {
+              ...recipe, // Returning the map as it is
+              // If ingredients dont exist then append an empty array instead of null
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
+      .subscribe(
+        responseList => {
+          console.log(this.recipeService);
+          this.recipeService.setRecipes(responseList);
+        },
+        error => {
+          console.log(error);
+        },
+        () => {
+          console.log('Fetched data from Server');
+        }
+      );
   }
 
   saveDataToServer() {
