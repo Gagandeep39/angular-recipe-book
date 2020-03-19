738c64334e0d1e42a978704d6990315023991cfe
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 02:39:12 +0530

    Added login method in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-interface AuthResponseData{
+interface AuthResponseData {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  // Added an Optional parameter, additional value recieved during login
+  registered?: string;
 }
 
 @Injectable({
@@ -14,14 +16,23 @@ interface AuthResponseData{
 })
 export class AuthService {
   apiKey = 'AIzaSyA-fSMTaEMpolwMgHKZgAQJQk9Qi3NeYxI';
-  authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  signUpUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
+  signInUrl =
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
 
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-
     // The response data will be an AuthResponseData object
-    return this.http.post<AuthResponseData>(this.authUrl + this.apiKey, {
+    return this.http.post<AuthResponseData>(this.signUpUrl + this.apiKey, {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    });
+  }
+
+  logIn(email: string, password: string) {
+    return this.http.post<AuthResponseData>(this.signInUrl + this.apiKey, {
       email: email,
       password: password,
       returnSecureToken: true
