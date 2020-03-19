0e4ff6179d85ff577887c9754e8578d2f6343821
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 00:44:27 +0530

    Added Injectable Reformatted Code

--- src/app/services/recipes-resolver.service.ts
+++ src/app/services/recipes-resolver.service.ts
@@ -1,14 +1,17 @@
 import { DataStorageService } from './data-storage.service';
 import { Recipe } from './../models/recipe.model';
-import { Resolve } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+@Injectable({providedIn: 'root'})
 export class RecipeResolverSerive implements Resolve<Recipe[]> {
   constructor(private dataStorageService: DataStorageService) {}
 
   resolve(
-    route: import('@angular/router').ActivatedRouteSnapshot,
-    state: import('@angular/router').RouterStateSnapshot
-  ): Recipe[] | import('rxjs').Observable<Recipe[]> | Promise<Recipe[]> {
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
     return this.dataStorageService.fetchDataFromServer();
   }
 }
