ec1ad4a954f2a1e5b3fa67b9df1e326a2bbed180
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-19 23:31:26 +0530

    Added a method to set data in service class list Reformatting of Code

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,42 +1,38 @@
-import { Injectable, Output, EventEmitter } from "@angular/core";
-import { Recipe } from "../models/recipe.model";
-import { Ingredient } from "../models/ingredient.model";
-import { ShoppingListService } from "./shopping-list.service";
-import { Subject } from "rxjs";
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
+import { Ingredient } from '../models/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class RecipeService {
-  
   // Use EventEmitter only for event binding between a child and parent component
   recipeSelected = new Subject<Recipe>();
-  // Import the component that recipe koist has changed 
+  // Import the component that recipe koist has changed
   recipeChanged = new Subject<Recipe[]>();
 
   recipes: Recipe[] = [
     new Recipe(
-      "Burger",
-      "A hamburger (also burger for short) is a food consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/428px-RedDot_Burger.jpg",
-      [
-        new Ingredient("Bread", 2),
-        new Ingredient("Cabbage", 1)
-      ]
+      'Burger',
+      'A hamburger (also burger for short) is a food consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/428px-RedDot_Burger.jpg',
+      [new Ingredient('Bread', 2), new Ingredient('Cabbage', 1)]
     ),
     new Recipe(
-      "Pasta",
-      "Pasta is a type of food typically made from an unleavened dough of durum wheat flour",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg/377px-%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg",
+      'Pasta',
+      'Pasta is a type of food typically made from an unleavened dough of durum wheat flour',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg/377px-%28Pasta%29_by_David_Adam_Kess_%28pic.2%29.jpg',
       [
-        new Ingredient("Wheat", 1),
-        new Ingredient("Eggs", 2),
-        new Ingredient("Flour", 1)
+        new Ingredient('Wheat', 1),
+        new Ingredient('Eggs', 2),
+        new Ingredient('Flour', 1)
       ]
     )
   ];
 
-  constructor(private shoppingListService : ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
   // Slice with no param returns a copy of an array
   // Without splice, the method will pass an array reference
@@ -46,7 +42,7 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  getRecipeById(index: number){
+  getRecipeById(index: number) {
     return this.recipes.slice()[index];
   }
 
@@ -55,7 +51,7 @@ export class RecipeService {
     this.recipeChanged.next(this.recipes.slice());
   }
 
-  addIngredientsToShoppingList(ingredients : Ingredient[]){
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addMultipleIngredients(ingredients);
   }
 
@@ -66,6 +62,12 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(responseList: Recipe[]) {
+    // this.recipes.push(...responseList);  // To append data to list 
+    this.recipes = responseList; // Replacing current list with new One
     this.recipeChanged.next(this.recipes.slice());
   }
 }
