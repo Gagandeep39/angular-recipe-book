ef500856fba813a8ca162fdf40f4eec7fa364091
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 00:57:45 +0530

    Added log messages in resolver

--- src/app/services/recipes-resolver.service.ts
+++ src/app/services/recipes-resolver.service.ts
@@ -1,10 +1,14 @@
 import { DataStorageService } from './data-storage.service';
 import { Recipe } from './../models/recipe.model';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class RecipeResolverSerive implements Resolve<Recipe[]> {
   constructor(private dataStorageService: DataStorageService) {}
 
@@ -12,6 +16,7 @@ export class RecipeResolverSerive implements Resolve<Recipe[]> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
+    console.log('Resolver was executed');
     return this.dataStorageService.fetchDataFromServer();
   }
 }
