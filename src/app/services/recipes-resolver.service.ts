5f9790d855e4d17752bb5d06cd1fb84259a3a46f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 01:18:19 +0530

    Added a comment for reference

--- src/app/services/recipes-resolver.service.ts
+++ src/app/services/recipes-resolver.service.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from 'src/app/services/recipe.service';
 import { DataStorageService } from './data-storage.service';
 import { Recipe } from './../models/recipe.model';
 import {
@@ -10,13 +11,26 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeResolverSerive implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
     console.log('Resolver was executed');
-    return this.dataStorageService.fetchDataFromServer();
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchDataFromServer();
+    } else {
+      return recipes;
+    }
   }
 }
+// HEre the resolver fetches from server everytime
+// To prevent this we will check if service already has data in it
+// In yes then the data will be returned else new data will  befetched
+// Resolver runs before Edit Componet
+// Value returned by resolver can be fetched from route.data.subscribe()
