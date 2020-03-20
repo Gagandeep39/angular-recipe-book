61cb9ebce7842998f755d46b34a1188dbfa89a1f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 23:30:21 +0530

    Generated component dynamically Using factory comonentresolver

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,15 @@
+import { PlaceholderDirective } from './../shared/placeholder.directive';
+import { AlertComponent } from './../shared/alert/alert.component';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthService, AuthResponseData } from './../services/auth.service';
 import { NgForm } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ComponentFactoryResolver,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-auth',
@@ -14,8 +21,18 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   // To display Error
   error: string = null;
+  // To bind a var with ng-template with property appPlaceHolder using 'type' binding
+  // This helper directive is created to manipulate the ng-template
+  // @ViewChild('customAlert') wasnt working
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
+  // Subscriptber to dynamic component
+  closeSubscriber: Subscription;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
 
   ngOnInit() {}
 
@@ -49,6 +66,8 @@ export class AuthComponent implements OnInit {
       error => {
         console.log(error);
         this.error = error;
+        // To diplay error Component programatically
+        this.showErrorAlert(error);
         this.isLoading = false;
       }
     );
@@ -57,5 +76,33 @@ export class AuthComponent implements OnInit {
 
   handleError() {
     this.error = null;
+  }
+
+  // Method that initializes the alert component programatically to display error
+  showErrorAlert(error: string) {
+    // Syntactically correct fo type script
+    // Wrong for angular, as it handles components differertly
+    // const alertComponent = new AlertComponent();
+    // Creating a component
+    const alertComponent = this.componentFactoryResolver.resolveComponentFactory(
+      AlertComponent
+    );
+    // Assigning a placeholder ng-template to a variable
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    // Clearing previous content from placeholder
+    hostViewContainerRef.clear();
+    // Creating a component inside placeholder
+    const tempComponent = hostViewContainerRef.createComponent(alertComponent);
+    // Mainpulating data of the component
+    // It is of type @input i.e we can sav data
+    tempComponent.instance.message = error;
+    // It is of @output i.e we can subscribe
+    this.closeSubscriber = tempComponent.instance.close.subscribe(() => {
+      // Here we cant call this method
+      // this.handleError();
+      // We will unsubscribe the sbscription from inside and clear the component
+      this.closeSubscriber.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
