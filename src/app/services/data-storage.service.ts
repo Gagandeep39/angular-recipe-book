5b117b136de292e761343ed3063f218071375c1b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 14:41:30 +0530

    MOdified fetch method to use creadential Fetch now fetches user credentai lfrom auth service Auth service token now passed as parameter to http request

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,8 +1,9 @@
+import { AuthService } from './auth.service';
 import { RecipeService } from './recipe.service';
 import { Recipe } from './../models/recipe.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpRequest, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,33 @@ import { map, tap } from 'rxjs/operators';
 export class DataStorageService {
   server_url = 'https://flutter-intro-setup-gagan.firebaseio.com/recipes.json';
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
+  // Fetch data returns an observable
+  // Ppipe is used to format data
+  // Take Reads one value from observable and unsubscribe i.e (auth subject observable)
+  // Valuue is then passed to another obserbale created inside exhaush map
+  // Exhaust map is used to combine 2 observables
+  // Http get request is now sent from second obervable (1st being the one fetching user credential)
+  // Http request now passes auth token as param to url (token recieved from auth subject observable)
+  // Http resonse is then mapped in a modifide key value pairs
+  // Outer map is an observable to map data
+  // Inner map execute the code for each elemnt in array to prepare key value pairs
+  // Tap ten sends the data to recipe servie class
   fetchDataFromServer() {
-    return this.http.get<Recipe[]>(this.server_url).pipe(
+    return this.authService.userCredential.pipe(
+      take(1),
+      exhaustMap(userData => {
+        return this.http.get<Recipe[]>(this.server_url,
+          {
+            // Parameter is the rule specified in firebase while accessing data
+            params: new HttpParams().set('auth', userData.token)
+          });
+      }),
       // To mprove the code and prevent breaking because of no ingredients(even though it ws working fine before)
       map(response => {
         console.log(response);
@@ -64,4 +88,4 @@ export class DataStorageService {
 // Tap method belongs to rxjs
 // It is usually used to read events of response data and update ui
 // The fetchData method() simply makes an Http class, its subscriber is in header component
-// Subscre was removed from service so that resolver can call the method 
+// Subscre was removed from service so that resolver can call the method
