c7a2ed8e33ba3311e9b72c00e82e001369217d91
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 12:40:35 +0530

    Created  user model to store credentials

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,16 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) {}
+
+  // Returns a token if it is valid
+  // Check is it exits or if expiry date is before current date
+  get token() {
+    if (this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return this._token;
+    }
+  }
+}
