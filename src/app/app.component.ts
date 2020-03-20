37f546eccf341d8aa4e548c0046f5c5641719107
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 18:20:52 +0530

    Auto login is performed in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { load } from '@angular/core/src/render3/instructions';
+import { AuthService } from './services/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
 
 }
