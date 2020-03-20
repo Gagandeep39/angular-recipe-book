b36fe3f6b3f4453348302cf3212c851dbb71204d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-20 18:18:01 +0530

    Fixed get token logic

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -9,8 +9,9 @@ export class User {
   // Returns a token if it is valid
   // Check is it exits or if expiry date is before current date
   get token() {
-    if (this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
-      return this._token;
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null;
     }
+    return this._token;
   }
 }
