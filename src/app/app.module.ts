f49d3d3df7e7cfd1ae212dcab85cccc030fe1e4c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-21 11:39:58 +0530

    Bundled recipe components into a module Added Recipes Module in imports of AppComponen mobule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { RecipesModule } from './recipes/recipes.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,34 +6,23 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceholderDirective } from './shared/placeholder.directive';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
@@ -44,7 +33,9 @@ import { PlaceholderDirective } from './shared/placeholder.directive';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    // Bundled all recipes related components in a mobule and imported it here
+    RecipesModule
   ],
   providers: [
     {
@@ -55,7 +46,7 @@ import { PlaceholderDirective } from './shared/placeholder.directive';
   ],
   bootstrap: [AppComponent],
   // Group of components that will be created programmatically
-  // Thesese compjents dont have a route or a selector 
+  // Thesese compjents dont have a route or a selector
   // 'selector: [appPlaceholder]' is not a selctor (its a directive)
   entryComponents: [
     AlertComponent
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,43 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+@NgModule({
+  declarations: [
+    // All components declared inside recpies Module are declared here
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  exports: [
+    // All the components that we want to use in other module are exported from her
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    // We need to exlicitly specify we use routing in this module
+    // Without thus, <router-outlet> wont be recognized
+    // We added it n root module, but everything works standlone so we have to specify it here too
+    RouterModule,
+    // We use directives such as (ngFor, ngIf etc)
+    BrowserModule,
+    // Added since we use reactive forms (formGroup, formArray)
+    ReactiveFormsModule
+    // We dd not need HttpModule because it provides ***services and those are available application wide
+  ]
+})
+export class RecipesModule {}
